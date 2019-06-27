63d0ffbe70a70795547ea592dea82fac7e9b09c8
adaao <armascaarelli@gmail.com>
2019-06-27 15:27:32 -0300

    pegando valor da tag em formato xml

--- src/main.py
+++ src/main.py
@@ -1,22 +1,17 @@
 import os.path
 from datetime import datetime
+'''from bs4 import BeautifulSoup'''
+from xml.dom import minidom
 
 pastaSat = 'C:/Program Files (x86)/Nox Automação/Fenix/SAT/XML'
 pastaNfe = 'C:/Program Files (x86)/Nox Automação/Fenix/NFe/XML'
-pastaDestino =
+pastaDestino = 'foo'
 
 def verificaSeAPastaExiste(caminho):
     if os.path.exists(caminho):
         return True
     else:
         return False
-
-if verificaSeAPastaExiste(pastaSat):
-    caminhos = [os.path.join(pastaSat, nome) for nome in os.listdir(pastaSat)]
-    arquivos = [arq for arq in caminhos if os.path.isfile(arq) and arq.lower().endswith('.xml')]
-    print('printando arquivos')
-    print(arquivos)
-
 
 
 def verificaArquivos():
@@ -25,7 +20,18 @@ def verificaArquivos():
     if now.month < 10:
         mesPassado = (str(now.year) + '0' + str(now.month - 1))
     else:
-        mesEAno = (str(now.year) + str(now.month - 1))
-    print(mesPassado)
+        mesPassado = (str(now.year) + str(now.month - 1))
+
+    if verificaSeAPastaExiste(pastaSat):
+        caminhos = [os.path.join(pastaSat, nome) for nome in os.listdir(pastaSat)]
+        arquivos = [arq for arq in caminhos if os.path.isfile(arq) and arq.lower().endswith('.xml')]
+
+    for arquivo in arquivos:
+        xmldoc = minidom.parse(arquivo)
+        dataDeEmissao = xmldoc.getElementsByTagName('dEmi')
+        print(arquivo)
+        print('data de emissao: ')
+        for x in dataDeEmissao:
+            print(x.toxml())
 
 verificaArquivos()
