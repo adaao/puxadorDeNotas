5c38a71d2c4346a97cb07a8bd850d35cbecb327f
= <=>
2019-07-28 15:39:16 -0300

    atualização no nome dos arquivos

--- scripts batch/importa tarefa em xml.bat
+++
@@ -1,3 +0,0 @@
-pause
-schtasks /Create /XML "C:\puxador de notas\tarefa que salva os arquivos fiscais.xml" /TN "teste de importacao por xml"
-pause
---
+++ scripts batch/importa_tarefa_em_xml.bat
@@ -0,0 +1,3 @@
+pause
+schtasks /Create /XML "C:\puxador de notas\tarefa_que_salva_os_arquivos_fiscais.xml" /TN "teste de importacao por xml"
+pause
